a74b570fa7607fcf5dce2684932c2c4a60307f27
Joshua Wu <joshua.wu.0910@gmail.com>
2020-04-11 18:50:10 -0700

    chore(index.ts): remove main.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-export {ics} from './main'
+interface iVEvent {
+	description: string;
+	duration: iDuration;
+	location: string;
+	subject: string;
+	timeEnd: iDate;
+	timeStart: iDate;
+}
+
+type iDuration = {
+	hours: number;
+	seconds: number;
+}
+
+type iDate = {
+	day: number;
+	month: number;
+	year: number;
+}
+
+const iCalx = (vEvent: iVEvent): string => {
+	console.log('ho')
+
+	const retVal: string[] = [
+		'BEGIN:VCALENDAR',
+		'VERSION: 2.0'
+	]
+
+
+	return vEvent.subject
+};
+
+export {iCalx};
